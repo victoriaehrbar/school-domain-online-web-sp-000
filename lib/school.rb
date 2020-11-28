4705ed18afbf774c759dd58578103b26b06eb2c1
victoriaehrbar <victoriaehrbar@gmail.com>
2020-11-28 22:45:04 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -15,7 +15,6 @@ class School
     roster[student_grade]
   end
 
-  # this method should arrange the students in each grade by alphabetical order
   def sort
     sorted = {}
     roster.each do |grade, students|
